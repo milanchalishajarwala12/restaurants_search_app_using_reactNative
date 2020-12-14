358a369e6683029d61b4671d0134ae51d2bac8a8
Milan Chalishajarwala <mchal4@unh.newhaven.edu>
2020-12-14 16:24:48 -0600

    Demo from new branch

--- Components/Details.js
+++ Components/Details.js
@@ -13,12 +13,12 @@ import {
 import { restaurantData } from '../getRestaurantsAPI';
 import { useNavigation } from '@react-navigation/native';
 
-export default function Details({ route }) {
-  const { name, reviews, ratings, imageUrl } = route.params;
-  return (
-    <View>
-      {useNavigation().setOptions({ title: name })}
-      <Text>{route.params.name}</Text>
-    </View>
-  );
-}
+// export default function Details({ route }) {
+//   const { name, reviews, ratings, imageUrl } = route.params;
+//   return (
+//     <View>
+//       {useNavigation().setOptions({ title: name })}
+//       <Text>{route.params.name}</Text>
+//     </View>
+//   );
+// }
